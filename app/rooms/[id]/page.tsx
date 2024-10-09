95e2c7e24775abb170290580dbff37ce22a88f2c
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-09 18:54:31 +0700

    Room Details Page

---
+++ app/rooms/[id]/page.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import { FaChevronCircleLeft } from "react-icons/fa";
+import Link from "next/link";
+import Heading from "@/components/Heading";
+import rooms from "@/data/rooms.json";
+import BookingForm from "@/components/BookingForm";
+
+type RoomPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const RoomPage = ({ params }: RoomPageProps) => {
+  const { id } = params;
+  const room = rooms.find((room) => room.$id === id);
+
+  if (!room) {
+    return <Heading title="Room Not Found" />;
+  }
+
+  return (
+    <>
+      <Heading title={room.name} />
+      <div className="bg-white shadow rounded-lg p-6">
+        <Link
+          href="/"
+          className="flex items-center text-gray-600 hover:text-gray-800 mb-4"
+        >
+          <FaChevronCircleLeft className="inline mr-1" />
+          <span className="ml-2">Back to Rooms</span>
+        </Link>
+
+        <div className="flex flex-col sm:flex-row sm:space-x-6">
+          <Image
+            src={`/images/${room.image}`}
+            alt={room.name}
+            className="w-full sm:w-1/3 h-64 object-cover rounded-lg"
+            width={400}
+            height={100}
+          />
+
+          <div className="mt-4 sm:mt-0 sm:flex-1">
+            <p className="text-gray-600 mb-4">{room.description}</p>
+
+            <ul className="space-y-2">
+              <li>
+                <span className="font-semibold text-gray-800">Size:</span>{" "}
+                {room.sqft}
+                sq ft
+              </li>
+              <li>
+                <span className="font-semibold text-gray-800">
+                  Availability:
+                </span>
+                {room.availability}
+              </li>
+              <li>
+                <span className="font-semibold text-gray-800">Price:</span>$
+                {room.price_per_hour}/hour
+              </li>
+              <li>
+                <span className="font-semibold text-gray-800">Address:</span>{" "}
+                {room.address}
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <BookingForm />
+      </div>
+    </>
+  );
+};
+
+export default RoomPage;
