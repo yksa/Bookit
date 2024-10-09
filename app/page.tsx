4c500aa3c4cb86fff15dea672cef51f6bf818715
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-09 18:36:26 +0700

    Heading Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <>
+      <Heading title="Available Rooms" />
       {rooms.length > 0 ? (
         rooms.map((room) => <RoomCard room={room} key={room.$id} />)
       ) : (
---
+++ components/Heading.tsx
@@ -0,0 +1,15 @@
+type HeadingProps = {
+  title: string;
+};
+
+const Heading = ({ title }: HeadingProps) => {
+  return (
+    <section className="bg-white mb-5 shadow px-4 py-4">
+      <h1 className="text-2xl font-bold tracking-tight text-gray-900">
+        {title}
+      </h1>
+    </section>
+  );
+};
+
+export default Heading;
