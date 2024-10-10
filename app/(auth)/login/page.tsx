6246df98db1ff9e3c87ccdd4048f5bd6e5dcf4e2
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-10 14:57:34 +0700

    feat(auth): add Login and Register pages with forms
    - Added Login page with a form for user authentication
    
    -  Added Register page with a form for user registration.
    
    -  Styled both forms with Tailwind CSS for a consistent look and feel.

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20">
+        <form>
+          <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
+            Login
+          </h2>
+
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="border rounded w-full py-3 px-4"
+              required
+            />
+          </div>
+
+          <div className="mb-8">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="border rounded w-full py-3 px-4"
+              required
+            />
+          </div>
+
+          <div className="flex flex-col gap-6">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-3 rounded hover:bg-blue-700"
+            >
+              Login
+            </button>
+
+            <p className="text-center">
+              No account?
+              <Link href="/register" className="text-blue-500">
+                {" "}
+                Register
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
